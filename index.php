f9cd9e2b4ee51466bfc3a2a61b79819add7ec788
Lucaponz91 <lucapo1991@msn.com>
2022-09-20 16:16:14 +0200

    step 1

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$testo = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde libero minus fugit dolorem. Sunt ducimus, dicta eligendi consequuntur itaque porro?';
+$parLength = strlen($testo)
+?>
+
+<p><?php echo $testo ?>  </p>
+<p>Il testo è lungo <?php echo $parLength?> caratteri.</p>
